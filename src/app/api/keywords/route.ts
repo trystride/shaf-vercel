7001dda3b76239b45ff33f0784629082a11617c1
abdullahkhaled818 <iigravity@gmail.com>
2024-12-19 04:10:16 +0300

    Fix Prisma imports to use prismaDb

--- src/app/api/keywords/route.ts
+++ src/app/api/keywords/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/libs/prisma";
+import { prisma } from "@/libs/prismaDb";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/libs/auth";
 
